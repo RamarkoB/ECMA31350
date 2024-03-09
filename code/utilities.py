b5d38c4b483d2e132be3c23d65447e897f586ede
David Xue <davidx94566@gmail.com>
2024-03-08 23:07:55 -0600

    utilities

--- code/utilities.py
+++ code/utilities.py
@@ -31,112 +31,160 @@ def convert_seconds(s, to='hours'):
         raise ValueError("Invalid 'to' value")
     
 
-def get_first_treat(repo, treatment):
-    repo = repo.replace("/", "--")
-    ts_str = treatment[repo]
+def get_first_treat(repo_id, treatment):
+    # repo = repo.replace("/", "--")
+    ts_str = treatment[repo_id]
     
     if ts_str is None:
         return 0
 
     dt = pd.to_datetime(ts_str)
     treat_time = dt.quarter + (dt.year - 2015) * 4
-    return 5 if treat_time <= 5 else treat_time
+
+    if treat_time <= 5:
+        return 5
+    elif treat_time > 12:
+        return 0
+    return treat_time
 
 
-def load_data(treat_func=None, quarter_range=None, outliers=None, level=0.95):
-    df = pd.read_csv("../githubData.csv").drop(columns=['Unnamed: 0', 'quarter'])
+def load_data(quarter_range=None, outliers=None, level=0.95,
+              filename='../data/issue-history/issue-history-all.csv'):
+    if filename == "../githubData.csv":
+        df = pd.read_csv(filename).drop(columns=['Unnamed: 0', 'quarter'])
+    else:
+        df = pd.read_csv(filename)
 
-    with open('../data/repo-names.json') as f:
-        repos = json.load(f)
-        repos = [r.replace("--", "/") for r in repos]
+    # with open('../data/repo-names.json') as f:
+    #     repos = json.load(f)
+    #     repos = [r.replace("--", "/") for r in repos]
+    repo_ids = []
 
-    with open('../data/treatment.json') as f:
-        treatment = json.load(f)
+    with open("../covariates/all-covariates.json") as f:
+        repo_ids.extend([repo['id'] for repo in json.load(f)])
 
-    df['opened'] = pd.to_datetime(df['opened'])
-    df['closed'] = pd.to_datetime(df['closed'])
-    df['timetoclose'] = pd.to_timedelta(df['timetoclose'])
+    with open('../data/treatment-all.json') as f:
+        treatment = {int(k): v for k, v in json.load(f).items()}
+
+    repo_ids = set(repo_ids).intersection(set(treatment.keys()))
+    df = df[df['repo_id'].isin(repo_ids)]
+    df['opened'] = pd.to_datetime(df['opened']).dt.tz_localize(None)
+    df['closed'] = pd.to_datetime(df['closed']).dt.tz_localize(None)
+    df['time'] = df['opened'].dt.quarter + (df['opened'].dt.year - 2015) * 4
+
+    if 'timetoclose' in df.columns:
+        df['timetoclose'] = pd.to_timedelta(df['timetoclose'])
+    else:
+        df['timetoclose'] = df['closed'] - df['opened']
 
     df['ttc_s'] = df['timetoclose'].dt.total_seconds()
-    if outliers == "winsor":
-        df['ttc_s'] = df['ttc_s'].clip(lower=0, upper=df['ttc_s'].quantile(level))
-    elif outliers == "drop":
-        df = df[df['ttc_s'] < df['ttc_s'].quantile(level)].copy()
-
-    df['ttc_h'] = df['ttc_s'] / 3600
-    df['ttc_d'] = df['ttc_s'] / 86400
-
-    df['repo'] = df['repository_url'].str.replace("https://api.github.com/repos/", "", regex=True).str.lower()
-    df['time'] = df['opened'].dt.quarter + (df['opened'].dt.year - 2015) * 4
-    df = df[df['repo'].isin(repos)]
-
-    first_treat_dict = {}
-    for repo in repos:
-        first_treat_dict[repo] = get_first_treat(repo, treatment)
-
-    df['first_treat'] = df['repo'].map(first_treat_dict)
-
-    if treat_func is None:
-        df['treat'] = np.where(df['first_treat'] == 5, 1, 0)
-    else:
-        df['treat'] = df.apply(treat_func, axis=1)
 
     if quarter_range is not None:
         df = df[df['time'].isin(quarter_range)]
     else:
         df = df[(df['time'] >= 1) & (df['time'] < 10)]
 
-    return df
+    if outliers == "winsor":
+        print(f"droppping at level: {convert_seconds(df['ttc_s'].quantile(level), to='days')}")
+        df['ttc_s'] = df['ttc_s'].clip(lower=0, upper=df['ttc_s'].quantile(level))
+    elif outliers == "drop":
+        print(f"droppping at level: {convert_seconds(df['ttc_s'].quantile(level), to='days')}")
+        df = df[df['ttc_s'] < df['ttc_s'].quantile(level)].copy()
+
+    df['ttc_h'] = df['ttc_s'] / 3600
+    df['ttc_d'] = df['ttc_s'] / 86400
+
+    if filename == "../githubData.csv":
+        df['repo_name'] = df['repository_url'].str.replace("https://api.github.com/repos/", "", regex=True).str.lower()
+    else:
+        df['repo_name'] = df['name'].str.lower()
+
+    first_treat_dict = {}
+    for repo_id in repo_ids:
+        first_treat_dict[repo_id] = get_first_treat(repo_id, treatment)
+
+    df['first_treat'] = df['repo_id'].map(first_treat_dict)
+    df['treat'] = np.where(df['first_treat'] == 5, 1, 0)
+
+    ## clean out repos with low sample size
+    gb_repo_time = (df[df['time'].isin([3, 4, 5, 6, 7])]
+                    .groupby(['repo_id', 'time'])['ttc_d']
+                    .count()
+                    .reset_index())
+    low_sample_ids = gb_repo_time[gb_repo_time['ttc_d'] < 10]['repo_id'].unique()
+
+    gb_repo = df.groupby(['repo_id']).agg({"time": set})
+    repo_times_dict = gb_repo.to_dict()['time']
+    no_time_ids = [k for k, v in repo_times_dict.items() if not set([4, 5, 6]) <= (v)]
+    drop_ids = list(low_sample_ids) + no_time_ids
+    print(f"dropping {len(drop_ids)} repos due to low sample size during treatment timeframe")
+
+    return df[~df['repo_id'].isin(drop_ids)]
 
 
 def get_y0(gb, repo_name, quarter, outcome):
     try:
-        return gb[(gb['repo'] == repo_name) & (gb['time'] == quarter)][outcome].values[0]
+        return gb[(gb['repo_id'] == repo_name) & (gb['time'] == quarter)][outcome].values[0]
     except:
         return np.nan
 
 
 def create_covs_dict():
-    with open("../data/covariates.json") as f:
-        covs = json.load(f)
+    cov_dict = {}
 
-    rv = {}
-    for repo_data in covs:
-        rv[repo_data['name'].lower()] = repo_data
+    # with open("../covariates/covariates.json") as f:
+    #     for data in json.load(f):
+    #         cov_dict[data['id']] = data
+
+    # with open("../covariates/covariates-old.json") as f:
+    #     for data in json.load(f):
+    #         cov_dict[data['id']] = data
+    with open("../covariates/all-covariates.json") as f:
+        for data in json.load(f):
+            cov_dict[data['id']] = data
     
-    return rv
+    return cov_dict
 
 
 def get_repo_stats(covariates, name, stat):
-    if name in covariates:
-        covs = covariates[name]
-        if stat == 'age':
-            return (pd.to_datetime("2016-02-17") - pd.to_datetime(covs['created_at']).tz_localize(None)).total_seconds() / 86400
-        return covs[stat]
+    # if name in covariates:
+    covs = covariates[name]
+    if stat == 'age':
+        return (pd.to_datetime("2016-02-17") - pd.to_datetime(covs['created_at']).tz_localize(None)).total_seconds() / 86400
+    if stat == 'has_wiki':
+        try:
+            return covs['has_wiki']
+        except:
+            return covs['hasWiki']
+        
+    return covs[stat]
     
-    owner, name = name.split("/")
-    with (open(f"../data/repos/{owner}--{name}")) as f:
-        data = json.load(f)
-        if stat == 'age':
-            return (pd.to_datetime("2016-02-17") - pd.to_datetime(data['created_at']).tz_localize(None)).total_seconds() / 86400
-        elif stat == 'lang':
-            return data['language']
-        elif stat == 'license':
-            return data['license']['name']
-        return data[stat]
+    # owner, name = name.split("/")
+    # with (open(f"../data/repos/{owner}--{name}")) as f:
+    #     data = json.load(f)
+    #     if stat == 'age':
+    #         return (pd.to_datetime("2016-02-17") - pd.to_datetime(data['created_at']).tz_localize(None)).total_seconds() / 86400
+    #     elif stat == 'lang':
+    #         return data['language']
+    #     elif stat == 'license':
+    #         return data['license']['name']
+    #     return data[stat]
 
 
-def get_grouped_df(df, outcome, time_range=None, first_tperiod=5, stats=None):
+def get_grouped_df(df, outcome, time_range=None, first_tperiod=5, stats=None, 
+                   agg_func='mean'):
     agg_dict = {
         'first_treat': 'first', 
         'treat': 'first',
-        outcome: 'mean',
+        # 'comment_count': 'mean', 
+        outcome: [agg_func, 'count'],
     }
 
-    gb = pd.DataFrame(df.groupby(['repo', 'time']).agg(agg_dict)).reset_index()
+    gb = pd.DataFrame(df.groupby(['repo_id', 'time']).agg(agg_dict)).reset_index()
+    gb.columns = [x[0] for x in list(gb.columns)[:-1]] + ['count']
 
-    repo_to_id = {repo: i + 1 for i, repo in enumerate(gb['repo'].unique())}
-    gb['id'] = gb['repo'].map(repo_to_id)
+    repo_to_id = {repo: i + 1 for i, repo in enumerate(gb['repo_id'].unique())}
+    gb['id'] = gb['repo_id'].map(repo_to_id)
     gb['post'] = np.where(gb['time'] >= first_tperiod, 1, 0)
 
     if time_range is not None:
@@ -145,31 +193,33 @@ def get_grouped_df(df, outcome, time_range=None, first_tperiod=5, stats=None):
         time_range = gb['time'].unique()
 
     for quarter in range(min(time_range), first_tperiod):
-        gb[f'y0_{quarter}'] = gb.apply(lambda x: get_y0(gb, x['repo'], quarter, outcome), axis=1)
+        gb[f'y0_{quarter}'] = gb.apply(lambda x: get_y0(gb, x['repo_id'], quarter, outcome), axis=1)
 
     # get repo-level statistics
     covs = create_covs_dict()
 
     if stats is None:
-        stats = ['age', 'size', 'watchers', 'lang', 'forks', 'license', 'has_wiki']
+        # stats = ['age', 'size', 'watchers', 'lang', 'forks', 'license', 'has_wiki']
+        stats = ['age', 'size', 'watchers', 'forks', 'has_wiki']
 
     for stat in stats:
-        gb[stat] = gb['repo'].map(lambda x: get_repo_stats(covs, x, stat))
+        gb[stat] = gb['repo_id'].map(lambda x: get_repo_stats(covs, x, stat))
 
     return gb
 
 
 def mean_gb(gb, outcome, wide=True):
-    covs = [col for col in gb.columns if col not in ['repo', 'time', 'post', outcome]]
+    covs = [col for col in gb.columns if col not in 
+            ['repo_id', 'time', 'post', outcome, 'count']]
     agg_dict = {col: 'first' for col in covs}
     agg_dict[outcome] = 'mean'
 
-    agged_gb = pd.DataFrame(gb.groupby(['repo', 'post']).agg(agg_dict)).reset_index()
+    agged_gb = pd.DataFrame(gb.groupby(['repo_id', 'post']).agg(agg_dict)).reset_index()
     
     if not wide:
         return agged_gb
     
-    wide_df = agged_gb.pivot(index=['repo'] + covs, columns='post', values=outcome)
+    wide_df = agged_gb.pivot(index=['repo_id'] + covs, columns='post', values=outcome)
     
     pivot_cols = [f"{outcome}{col}" for col in wide_df.columns]
     wide_df.columns = pivot_cols
@@ -217,6 +267,8 @@ def plot_trends(df, outcome, ranges=None):
     plt.axvline(4.75, color="k", linestyle="--", label="Treatment")
     handles, _ = ax.get_legend_handles_labels()
     plt.legend(
-        np.array(handles)[[-1, -2, -3]], ["Treatment", "Treated", "Controls"], frameon=False
+        np.array(handles)[[-1, -2, -3]], ["Treatment", "Treated", "Controls"], 
+        frameon=False,
+        loc="upper left"
     )
     plt.show()
